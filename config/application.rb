aa11459bccccb5faae26bc5dc88ec8b098968599
Minh Quang <minhquang4334@gmail.com>
2019-07-23 20:25:15 +0700

    Add Submodule Vue for handle Client Side in Rails App -- remove comment

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,12 @@ Bundler.require(*Rails.groups)
 module Myapp
   class Application < Rails::Application
     config.load_defaults 5.2
-
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
     I18n.load_path += Dir[Rails.root.join("lib", "locale", "*.{rb,yml}")]
     I18n.available_locales = [:en, :vi]
     I18n.default_locale = :en
---
+++ food-order/src/components/home/MainContent.vue
@@ -0,0 +1,93 @@
+<template>
+    <div id="particles" class="gtco-section">
+        <div class="gtco-container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+                    <h2 class="cursive-font">Today Menu</h2>
+                    <p>Lee Sin Restaurant</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="/static/images/img_1.jpg" class="fh5co-card-item image-popup">
+                        <figure>
+                            <div class="overlay"><i class="ti-plus"></i></div>
+                            <img src="/static/images/img_1.jpg" alt="Image" class="img-responsive">
+                        </figure>
+                        <div class="fh5co-text">
+                            <h2>Fresh Mushrooms</h2>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            <p><span class="price cursive-font">$19.15</span></p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="/static/images/img_2.jpg" class="fh5co-card-item image-popup">
+                        <figure>
+                            <div class="overlay"><i class="ti-plus"></i></div>
+                            <img src="/static/images/img_2.jpg" alt="Image" class="img-responsive">
+                        </figure>
+                        <div class="fh5co-text">
+                            <h2>Cheese and Garlic Toast</h2>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            <p><span class="price cursive-font">$20.99</span></p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="/static/images/img_3.jpg" class="fh5co-card-item image-popup">
+                        <figure>
+                            <div class="overlay"><i class="ti-plus"></i></div>
+                            <img src="/static/images/img_3.jpg" alt="Image" class="img-responsive">
+                        </figure>
+                        <div class="fh5co-text">
+                            <h2>Grilled Chiken Salad</h2>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            <p><span class="price cursive-font">$8.99</span></p>
+
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="/static/images/img_4.jpg" class="fh5co-card-item image-popup">
+                        <figure>
+                            <div class="overlay"><i class="ti-plus"></i></div>
+                            <img src="/static/images/img_4.jpg" alt="Image" class="img-responsive">
+                        </figure>
+                        <div class="fh5co-text">
+                            <h2>Organic Egg</h2>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            <p><span class="price cursive-font">$12.99</span></p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="/static/images/img_5.jpg" class="fh5co-card-item image-popup">
+                        <figure>
+                            <div class="overlay"><i class="ti-plus"></i></div>
+                            <img src="/static/images/img_5.jpg" alt="Image" class="img-responsive">
+                        </figure>
+                        <div class="fh5co-text">
+                            <h2>Tomato Soup with Chicken</h2>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            <p><span class="price cursive-font">$23.10</span></p>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <a href="/static/images/img_6.jpg" class="fh5co-card-item image-popup">
+                        <figure>
+                            <div class="overlay"><i class="ti-plus"></i></div>
+                            <img src="/static/images/img_6.jpg" alt="Image" class="img-responsive">
+                        </figure>
+                        <div class="fh5co-text">
+                            <h2>Salad with Crispy Chicken</h2>
+                            <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                            <p><span class="price cursive-font">$5.59</span></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
