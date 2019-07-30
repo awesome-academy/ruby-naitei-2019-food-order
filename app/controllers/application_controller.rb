8dde1c9483505f528b97c7e04a43f9324e334141
BlazingRockStorm <rocker.gryphon@gmail.com>
2019-07-30 10:00:36 +0700

    login as admin and fix some convention

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  protect_from_forgery with: :exception
+
+  include AdminSessionsHelper
 
   private
 
@@ -9,6 +12,12 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     {locale: I18n.locale}
+  end
+
+  def logged_in_admin
+    return if logged_in?
+    flash[:danger] = t "flash.please_login"
+    redirect_to admin_login_url
   end
 
   helper_method :current_user
