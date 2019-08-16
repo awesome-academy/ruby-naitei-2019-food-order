92ea9c0ffb2b5b971562d7f8fcb3720a89e97791
quanhd-0035 <hoang.duc.quan@sun-asterisk.com>
2019-08-16 10:19:03 +0700

    add validate email domain

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,8 +7,17 @@ RSpec.describe User, type: :model do
     @user3 = create(:user, first_name: "Bill", last_name: "Foster")
     @user4 = create(:user, first_name: "John", last_name: "Doe")
   end
-  
+
   it "return a sorted by name array of result that match" do
     User.order_by_name.should == [@user2, @user3, @user4, @user1]
+  end
+    
+  it "is valid with valid attributes" do
+    expect(@user1).to be_valid
+  end
+
+  it "is not valid without a probably email" do
+    @user2.update_column(:email, "makarov@gmail.com")
+    expect(@user2).to_not be_valid
   end
 end
